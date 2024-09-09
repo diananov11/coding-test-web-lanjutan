3edec448045b73d53f00ad480f05a7806ade7cf0
diananov11 <ohlemon0678@gmail.com>
2024-09-09 20:28:36 +0700

    done 15 menit

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const calculateArray = (array) => {
   let reverse = array.reverse();
-  reverse.map(item => item * 2)
+  let map = reverse.map(item => item * 2);
+  return map;
 }
 
-console.log(calculateArray([1, 2, 3]));
+console.log(calculateArray([1, 4, 6, 7] ));
+console.log(calculateArray([7, 4, 2, 1] ));
+console.log(calculateArray([5]));
+console.log(calculateArray([1, 3]));
+
+
